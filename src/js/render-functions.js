0ea5bd6d2cc1862215289e836cb2dc81518f852b
dashakotets <dashacot1@gmail.com>
2024-08-30 15:12:55 +0100

    ul upgraded

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -11,16 +11,10 @@ export const createGalleryCardTemplate = card => {
     <a href="${card.largeImageURL}"><img src="${card.previewURL}" alt="" title="" class="gallery-img"/></a>
     <div class="card-info">
     <ul class="info-text">
-    <li>Likes</li>
-    <li>Views</li>
-    <li>Comments</li>
-    <li>Downloads</li>
-    </ul>
-    <ul class="info">
-    <li>${card.likes}</li>
-    <li>${card.views}</li>
-    <li>${card.comments}</li>
-    <li>${card.downloads}</li>
+    <li>Likes<span>${card.likes}</span></li>
+    <li>Views<span>${card.views}</span></li>
+    <li>Comments<span>${card.comments}</span></li>
+    <li>Downloads<span>${card.downloads}</span></li>
     </ul>
 </div></div>`;
 };
